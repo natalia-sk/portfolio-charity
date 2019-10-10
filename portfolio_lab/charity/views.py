486d2e8955d7acfe88866cc808e8133df8fcf8cf
Natalia Szokaluk-Krawczyk <nataliaszokaluk@gmail.com>
2019-10-10 23:29:27 +0200

    Preparation of templates.

--- portfolio_lab/charity/views.py
+++ portfolio_lab/charity/views.py
@@ -5,6 +5,20 @@ from django.views import View
 
 
 class LandingPage(View):
-
     def get(self, request):
-        return render(request, 'index.html')
+        return render(request, 'charity/index.html')
+
+
+class AddDonation(View):
+    def get(self, request):
+        return render(request, 'charity/form.html')
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, 'charity/login.html')
+
+
+class Register(View):
+    def get(self, request):
+        return render(request, 'charity/register.html')
