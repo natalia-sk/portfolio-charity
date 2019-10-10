9cdf2f4ea39f69edfd78293fbb92e06a4665a6f2
Natalia Szokaluk-Krawczyk <nataliaszokaluk@gmail.com>
2019-10-10 23:48:27 +0200

    Created 'Institution' model.

--- portfolio_lab/charity/models.py
+++ portfolio_lab/charity/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+TYPES = (
+    (1, 'fundacja'),
+    (2, 'organizacja pozarządowa'),
+    (3, 'zbiórka lokalna')
+)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    type_of = models.IntegerField(choices=TYPES, default=1)
+    categories = models.ManyToManyField(Category)
