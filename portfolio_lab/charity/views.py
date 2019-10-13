53e447c178567d8675c8551c95299614283dec1d
Natalia Szokaluk-Krawczyk <nataliaszokaluk@gmail.com>
2019-10-13 20:30:36 +0200

    Changed imports in views.py.

--- portfolio_lab/charity/views.py
+++ portfolio_lab/charity/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
 from django.views import View
